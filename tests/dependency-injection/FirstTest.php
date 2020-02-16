5c19e26738cf4b0febf12cf16953bd4114fd198c
Timo Poppinga <timo@silvermoon.tech>
2020-02-16 12:09:09 +0100

    add first test

---
+++ tests/dependency-injection/FirstTest.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+
+namespace SilvermoonTests\DependencyInjection;
+
+use PHPUnit\Framework\TestCase;
+use Silvermoon\TestingFramework\BaseUnitTest;
+
+class FirstTest extends BaseUnitTest
+{
+
+}
