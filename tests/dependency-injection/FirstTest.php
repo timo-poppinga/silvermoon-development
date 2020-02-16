1ac2752eadb7b8e2b8182f39308b9007758cf604
Timo Poppinga <timo@silvermoon.tech>
2020-02-16 12:11:27 +0100

    add fix code style

--- tests/dependency-injection/FirstTest.php
+++ tests/dependency-injection/FirstTest.php
@@ -3,10 +3,8 @@ declare(strict_types=1);
 
 namespace SilvermoonTests\DependencyInjection;
 
-use PHPUnit\Framework\TestCase;
 use Silvermoon\TestingFramework\BaseUnitTest;
 
 class FirstTest extends BaseUnitTest
 {
-
 }
