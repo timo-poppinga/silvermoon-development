1eb3e7fd18771c77d43d63e974a1e0c76cec1729
Timo Poppinga <timo@silvermoon.tech>
2020-05-24 14:28:45 +0200

    fix php stan

--- packages/component/configuration/src/Helper/_DirectoryHelper.php
+++ packages/component/configuration/src/Helper/_DirectoryHelper.php
@@ -3,6 +3,8 @@ declare(strict_types=1);
 
 namespace Silvermoon\Configuration\Helper;
 
+use Silvermoon\Exception\System\PathNotFoundException;
+
 /**
  * Class _DirectoryHelper
  */
@@ -12,11 +14,15 @@ class _DirectoryHelper
      * @param string $path
      * @param int $depth
      * @param int $startAtDepth
-     * @return array
+     * @return array<string>
+     * @throws PathNotFoundException
      */
     public static function directoriesInPath(string $path, int $depth = 0, int $startAtDepth = 0): array
     {
         $realpathPath = \realpath($path);
+        if($realpathPath === false) {
+            throw new PathNotFoundException();
+        }
         $directories = self::directoriesInPathAbsolutePath($realpathPath, $depth, $startAtDepth);
         $relativeDirectories = [];
         foreach ($directories as $directory) {
@@ -29,11 +35,15 @@ class _DirectoryHelper
      * @param string $path
      * @param int $depth
      * @param int $startAtDepth
-     * @return array
+     * @return array<string>
+     * @throws PathNotFoundException
      */
     public static function directoriesInPathAbsolutePath(string $path, int $depth = 0, int $startAtDepth = 0): array
     {
         $realpathPath = \realpath($path);
+        if($realpathPath === false) {
+            throw new PathNotFoundException();
+        }
         return self::_directoriesInPath($realpathPath, $depth, $startAtDepth);
     }
 
@@ -41,19 +51,26 @@ class _DirectoryHelper
      * @param string $path
      * @param int $depth
      * @param int $startAtDepth
-     * @param bool $absolutePath
-     * @param int $currentDeep
+     * @param int $currentDepth
      * @return string[]
+     * @throws PathNotFoundException
      */
     protected static function _directoriesInPath(string $path, int $depth = 0, int $startAtDepth = 0, int $currentDepth = 0): array
     {
         $directories = [];
         if (\is_dir($path)) {
             $directoryNames = scandir($path);
+            if($directoryNames === false) {
+                throw new PathNotFoundException();
+            }
             foreach ($directoryNames as $directoryName) {
                 if (\is_dir($path . '/' . $directoryName) && $directoryName !== '..' && $directoryName !== '.') {
                     if ($startAtDepth <= $currentDepth) {
-                        $directories[] = \realpath($path . '/' . $directoryName);
+                        $realPath = \realpath($path . '/' . $directoryName);
+                        if($realPath === false) {
+                            throw new PathNotFoundException();
+                        }
+                        $directories[] = $realPath;
                     }
                     if ($currentDepth < $depth) {
                         $subDirectories = self::_directoriesInPath($path . '/' . $directoryName, $depth, $startAtDepth, $currentDepth + 1);
